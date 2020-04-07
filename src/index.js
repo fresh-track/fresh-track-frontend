902158277e07259ad5f85c677ddcdd31abc0994f
Michael Romay <amerror@gmail.com>
2020-04-06 18:30:07 -0700

    mic visualizer working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Waveform from './Waveform'
+import App from './App';
+// import Waveform from './Waveform'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Waveform />
+    <App />
+    {/* <Waveform /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
