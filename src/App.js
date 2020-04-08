cf0c1a86dda15f128ee5de49ad9bb68fb5097d96
cficht <chris.ficht@gmail.com>
2020-04-08 14:18:02 -0700

    updated login page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import Waveform from './Waveform.js';
 import Login from './Login';
+import PrivateRoute from './PrivateRoute.js';
 // import Header from './Header.js';
 // import './App.css';
 import { 
     BrowserRouter, 
     Route,
-    Redirect,
+    Switch
  } from 'react-router-dom';
 
-const isLoggedIn = () => JSON.parse(localStorage.getItem('user'));
-
 export default class App extends React.Component {
+  state = { user: JSON.parse(localStorage.getItem('user')) };
+
+  setUser = user => {
+    this.setState({ user: user.body })
+  }
+
   render () {
     return(
     <div className="App">
-
         <BrowserRouter>
-            <Route path="/" render={() =>
-            isLoggedIn()
-                ? <Waveform />
-                : <Redirect to='/login' />  
-          }/>
-            <Route path='/login' component={Login} />       
+            <Switch>
+            <PrivateRoute exact path="/" component={Waveform} user={this.state.user}/>
+            <Route exact path="/login" render={(props) => <Login {...props} setUser={this.setUser} user={this.state.user}/>}  />
+          </Switch>     
         </BrowserRouter>
     </div>
-/* <div className="App">
-
-<BrowserRouter>
-    <Route path="/" render={() =>
-     <Redirect to='/login' />  
-  }/>
-    <Route path='/login' component={Login} />       
-</BrowserRouter>
-</div>  */
     );
   }
 }
