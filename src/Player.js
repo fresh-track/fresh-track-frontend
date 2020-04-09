01ae27b24b01aa29901134134b90f78446ef8d45
Michael Romay <amerror@gmail.com>
2020-04-08 19:30:12 -0700

    trying out drive map

--- src/Player.js
+++ src/Player.js
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactJkMusicPlayer from "react-jinke-music-player";
 // import { saveAs } from 'file-saver';
 import "react-jinke-music-player/assets/index.css";
-import AudioAnalyser from './AudioAnalyser';
+// import AudioAnalyser from './AudioAnalyser';
 import './Player.css';
 
 let bandcampArr = [1413157771, 4037375649, 2926175440, 4267872102, 2358433489, 3535544007];
+let driveArr = ['1iSDrOQH7IG8OFczKrVX19IyAlO5HNDfX', '135T3wOSF_o6VMamNfmrgt9kfTbVucWDE', '1grL_7KMiGpzAJU4jP6dBLq-9RzfyD70l'];
 
 //function to conditionally render bandcamp iframes only if there are items in the bandcampArr
 function bandcampRender(){
@@ -17,28 +18,49 @@ function bandcampRender(){
   }
 };
 
-const audioList1 = [
-  // {
-  //   name: 'push',
-  //   singer: 'budge',
-  //   cover: 'https://emby.media/community/uploads/inline/355992/5c1cc71abf1ee_genericcoverart.jpg',
-  //   musicSrc: () => {
-  //     return Promise.resolve(
-  //       'https://drive.google.com/u/0/uc?id=1iSDrOQH7IG8OFczKrVX19IyAlO5HNDfX&export=download'
-  //     )
-  //   },
-  // },
-  {
-    name: 'Despacito',
-    singer: 'Luis Fonsi',
+function driveRender(){
+  if(driveArr.length > 0){
+    return driveArr.map((value, index) => {
+      return {
+        name: 'push',
+        singer: 'budge',
+        cover: 'https://emby.media/community/uploads/inline/355992/5c1cc71abf1ee_genericcoverart.jpg',
+        musicSrc: () => {
+          return Promise.resolve(
+            'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3')
+      },
+    }})
+  }
+};
+
+// `https://drive.google.com/u/0/uc?id={value}&export=download`
+
+// console.log(driveRender())
+const audioList1 = [driveRender()]
+console.log(audioList1);
+
+// const audioList1 = [
+//   {
+//     name: 'push',
+//     singer: 'budge',
+//     cover: 'https://emby.media/community/uploads/inline/355992/5c1cc71abf1ee_genericcoverart.jpg',
+//     musicSrc: () => {
+//       return Promise.resolve(
+//         'https://drive.google.com/u/0/uc?id=1iSDrOQH7IG8OFczKrVX19IyAlO5HNDfX&export=download'
+//       )
+//     },
+//   },
+//   {
+//     name: 'Despacito',
+//     singer: 'Luis Fonsi',
     
-    musicSrc: () => {
-      return Promise.resolve(
-        'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3'
-      )
-    },
-  },
-]
+//     musicSrc: () => {
+//       return Promise.resolve(
+//         'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3'
+//       )
+//     },
+//   },
+// ]
 
 const options = {
   audioLists: audioList1,
@@ -60,7 +82,7 @@ export default class Player extends React.Component {
   }
   componentDidMount() {
     const audio = document.querySelector('audio')
-    console.log(audio)
+    // console.log(audio)
     this.setState({audio})
   }
 
