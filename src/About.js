0d43b73c948615a064f02252053247fb159b99b6
Will G Piro <willgpiro@gmail.com>
2020-04-08 20:48:42 -0700

    module error fix

--- src/About.js
+++ src/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default class About extends Component {
@@ -8,7 +7,7 @@ export default class About extends Component {
     return (
       <div className="about-container">
         
-        <h1> ABOUT OUT TEAM </h1>
+        <h1> The Fresh Track Trio </h1>
 
         <p> 
         The Fresh Track team is brought to you by a dev trio dedicated to brining music lovers a new kind of music repository for artists to upload, share, and collaborate on their unreleased music tracks from their Soundcloud, Bandcamp, Google Drive and Drop Box music libraries. 
@@ -18,40 +17,38 @@ export default class About extends Component {
 
           <div className="photo">
             <h2>Chris Ficht</h2>
-                <img src='Chris_Ficht.jpeg' alt="Chris" />
-                <div className="socialicons">
-                <a href="https://www.linkedin.com/in/chrisficht/" className="Linkedin social" >
-                <FontAwesomeIcon icon={faLinkedin} size="2x" /> </a>
-                <a href="https://github.com/cficht" className="GitHub social" >
-                <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+                <img src='Chris_Ficht.jpg' alt="Chris" />
+                  <div className="socialicons">
+                    <a href="https://www.linkedin.com/in/chrisficht/" className="Linkedin social" >
+                       <FontAwesomeIcon icon={faLinkedin} size="2x" /> </a>
+                    <a href="https://github.com/cficht" className="GitHub social" >
+                       <FontAwesomeIcon icon={faGithub} size="2x" /></a>
                 </div>
-                    </div>            
+            </div>            
 
-                    <div className="photo">
-                        <h2>Mikey Romay</h2>
-                        <img src='Mikey_Romay.jpeg' alt="Mikey Romay" />
-
-                        <div className="socialicons">
-                            <a href="https://www.linkedin.com/in/michaelromay/" className="Linkedin social" >
-                                <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-                            <a href="https://github.com/mikeymasonic" className="GitHub social">
-                                <FontAwesomeIcon icon={faGithub} size="2x" /></a>
-                        </div>
-                    </div>
-
-                    <div className="photo">
-                        <h2>Will Piro</h2>
-                        <img src='Will_Piro.jpeg' alt="Will" />
-
-                        <div className="socialicons">
-                            <a href="https://www.linkedin.com/in/willpiro/" className="Linkedin social">
-                                <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-                            <a href="https://github.com/shaka2pass" className="GitHub social">
-                                <FontAwesomeIcon icon={faGithub} size="2x" /></a>
-                        </div>
-                    </div>
+          <div className="photo">
+              <h2>Mikey Romay</h2>
+                <img src='Mikey_Romay.jpg' alt="Mikey Romay" />
+                  <div className="socialicons">
+                    <a href="https://www.linkedin.com/in/michaelromay/" className="Linkedin social" >
+                      <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+                    <a href="https://github.com/mikeymasonic" className="GitHub social">
+                      <FontAwesomeIcon icon={faGithub} size="2x" /></a>
                 </div>
-              </div>
+          </div>
+
+          <div className="photo">
+              <h2>Will Piro</h2>
+                <img src='Will_Piro.jpg' alt="Will" />
+                  <div className="socialicons">
+                    <a href="https://www.linkedin.com/in/willpiro/" className="Linkedin social">
+                      <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+                    <a href="https://github.com/shaka2pass" className="GitHub social">
+                      <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+                    </div>
+                  </div>
+        </div>
+      </div>
                    
     )
   }
