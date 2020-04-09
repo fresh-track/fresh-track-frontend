013776ea6bc1c5b45a33dba37635573987c64d8b
Michael Romay <amerror@gmail.com>
2020-04-08 18:50:23 -0700

    nav bar stuff

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import './Header.css';
 
 export default class Header extends Component {
+  // logOutButton = () => {
+  //   if (this.props.user) return <div className="login-div"><Button variant="contained" color="primary" size="small" className="button" onClick={e => this.handleLogOut()}>Logout</Button></div>;
+  // }
+
   logOutButton = () => {
-    if (this.props.user) return <div className="login-div"><Button variant="contained" color="secondary" size="small" className="button" onClick={e => this.handleLogOut()}>Logout</Button></div>;
+    if (this.props.user) return <div className="login-div"><Link onClick={e => this.handleLogOut()}>/logout/</Link></div>;
   }
 
   handleLogOut = () => {
@@ -16,11 +20,13 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <div className="main-title">fresh-track</div>
-        <Link to='/'>player</Link> <></>
-        <Link to='/profile'>profile</Link> <></>
-        <Link to='/about'>about</Link> <></>
+      <div className="main-title">fresh-track</div>
+      <div className="navBar">
+        <Link to='/'>/player/</Link> <></>
+        <Link to='/profile'>/profile/</Link> <></>
+        <Link to='/about'>/about/</Link> <></>
         {this.logOutButton()}
+      </div>
       </div>
     )
   }
