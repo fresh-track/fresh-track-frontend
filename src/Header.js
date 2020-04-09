c4c5a97b6498c0097878fc6c42b0b8bb46e48bd5
Michael Romay <amerror@gmail.com>
2020-04-08 18:14:09 -0700

    log out

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import './Header.css';
 
 export default class Header extends Component {
+  logOutButton = () => {
+    if (this.props.user) return <div className="login-div"><Button variant="contained" color="secondary" size="small" className="button" onClick={e => this.handleLogOut()}>Logout</Button></div>;
+}
+
+  handleLogOut = () => {
+      localStorage.clear();
+      this.props.setUser({ body: null });
+      this.props.history.push('/');
+  }
   render() {
     return (
       <div>
@@ -10,6 +20,7 @@ export default class Header extends Component {
         <Link to='/'>player</Link> <></>
         <Link to='/profile'>profile</Link> <></>
         <Link to='/about'>about</Link> <></>
+        {this.logOutButton()}
       </div>
     )
   }
