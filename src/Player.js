890fef992d4af638e9918647b7f2467bb9f4c7ce
Michael Romay <amerror@gmail.com>
2020-04-09 09:35:16 -0700

    thing works

--- src/Player.js
+++ src/Player.js
@@ -7,9 +7,9 @@ import "react-jinke-music-player/assets/index.css";
 import './Player.css';
 
 let bandcampArr = [1413157771, 4037375649, 2926175440, 4267872102, 2358433489, 3535544007];
-// let driveArr = ['1iSDrOQH7IG8OFczKrVX19IyAlO5HNDfX', '135T3wOSF_o6VMamNfmrgt9kfTbVucWDE', '1grL_7KMiGpzAJU4jP6dBLq-9RzfyD70l'];
+let driveArr = ['1iSDrOQH7IG8OFczKrVX19IyAlO5HNDfX', '135T3wOSF_o6VMamNfmrgt9kfTbVucWDE', '1grL_7KMiGpzAJU4jP6dBLq-9RzfyD70l'];
 
-let driveArr = [1, 2, 3];
+// let driveArr = [1, 2, 3];
 //function to conditionally render bandcamp iframes only if there are items in the bandcampArr
 function bandcampRender(){
   if(bandcampArr.length > 0){
@@ -28,7 +28,7 @@ function driveRender(){
         cover: 'https://emby.media/community/uploads/inline/355992/5c1cc71abf1ee_genericcoverart.jpg',
         musicSrc: () => {
           return Promise.resolve(
-            'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3')
+            `https://drive.google.com/u/0/uc?id=${value}&export=download`)
       },
     }})
   }
@@ -37,7 +37,7 @@ function driveRender(){
 // `https://drive.google.com/u/0/uc?id={value}&export=download`
 
 // console.log(driveRender())
-const audioList1 = [driveRender()]
+const audioList1 = driveRender()
 console.log(audioList1);
 
 // const audioList1 = [
