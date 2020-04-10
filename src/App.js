98210a686ca724858e89f592e891b3bc788842f4
Will G Piro <willgpiro@gmail.com>
2020-04-10 09:59:12 -0700

    splash page update with logo and description

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 import React from 'react';
 // import Player from './Player';
+import Home from './Home';
 import Login from './Login';
 import Header from './Header';
 import Profile from './Profile';
@@ -29,6 +30,7 @@ export default class App extends React.Component {
         <BrowserRouter>
         <Header user={this.state.user} setUser={this.setUser}/>
             <Switch>
+            <Route exact path ="/" component ={Home} />
             <PrivateRoute exact path="/player/:friendId?" component={DrivePlayer} user={this.state.user}/>
             <Route exact path="/login" render={(props) => <Login {...props} setUser={this.setUser} user={this.state.user}/>}  />
             <PrivateRoute exact path='/profile' component={Profile} user={this.state.user}/>
