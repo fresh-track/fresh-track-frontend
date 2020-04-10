316b72c8f1cfa11f3911f71737cc6029ec7418d2
cficht <chris.ficht@gmail.com>
2020-04-10 12:47:33 -0700

    populate folders

--- src/Profile.js
+++ src/Profile.js
@@ -5,6 +5,8 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import UserFolder from './UserFolder';
+import FriendFolder from './FriendFolder';
 
 function Alert(props) {
   return <MuiAlert elevation={10} variant="filled" {...props} />;
@@ -17,7 +19,9 @@ export default class Profile extends Component {
     bandcampUsername: '',
     driveFolder: '',
     friend: '',
-    open: false
+    open: false,
+    userFolders: [],
+    friendFolders: []
 }
 
 closeSB = (event, reason) => {
@@ -51,11 +55,36 @@ handleAddFriend = async () => {
   }
 }
 
+handleGetFolders = async (userInfo) => {
+  let friendFolders = [];
+  const user = await request.get(`${process.env.REACT_APP_DB_URL}/api/v1/user/${userInfo._id}`).withCredentials();
+  this.setState({ userFolders: user.body.drives })
+  const userMatch = await request.get(`${process.env.REACT_APP_DB_URL}/api/v1/user/friend-folders`).withCredentials();
+  userMatch.body.forEach(userM => {
+    if (userM._id === userInfo._id) {
+      friendFolders = userM.friendFolders;
+    }
+  })
+  this.setState({ friendFolders: friendFolders })
+}
+
 componentDidMount() {
   this.setState({ user: this.props.user })
+  this.handleGetFolders(this.props.user)
 }
 
   render() {
+    const userFolderNodes = this.state.userFolders.map(folder => <UserFolder folder={folder}></UserFolder>);
+  const friendFolderNodes = this.state.friendFolders.map(folder => { 
+    console.log(folder.userRef);
+    console.log(this.state.user.followedUsers)
+    let friendName;
+    this.state.user.followedUsers.forEach(followedUser => {
+      console.log(followedUser.friendId)
+      if(followedUser.friendId === folder.userRef) friendName = followedUser.friend
+    }) 
+    return <FriendFolder folder={folder} friendName={friendName}></FriendFolder> 
+  });
     return (
       <div className="profilediv">
         <div className="updatediv">
@@ -70,6 +99,14 @@ componentDidMount() {
         <div className="updatediv">
           <TextField id="standard-basic" label="Add Friend" type="text"  value={this.state.friend} onChange={(e) => this.setState({ friend: e.target.value })} />
           <Button variant="contained" color="primary" size="small" className="button" onClick={this.handleAddFriend}>Add</Button>
+        </div>
+        <div className="userfolderdiv">
+          <h2>{this.state.user.username}'s Folders</h2>
+          {userFolderNodes}
+        </div>
+        <div className="userfolderdiv">
+          <h2>Friend Folders</h2>
+          {friendFolderNodes}
         </div>
         <Snackbar open={this.state.open} autoHideDuration={6000} onClose={this.closeSB}>
                     <Alert onClose={this.closeSB} severity="warning">User does not exist</Alert>
