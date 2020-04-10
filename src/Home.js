27af7dda326ee6cd8b4ce524dff1832f80d3184b
Will G Piro <willgpiro@gmail.com>
2020-04-10 11:32:57 -0700

    splash page fixes

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Home.css';
@@ -6,24 +7,20 @@ import './Home.css';
 export default class Home extends Component {
   render() {
     return (
+      
       <div className="Home-container">
         
-        <h1>fresh-track</h1>
+        <h1 className ="company-name">fresh-track</h1>
         
-        <p> 
-         Fresh-track is a unique file repository that allows artists to listen to, share, and collaborate by collecting their unreleased and released music from their Soundcloud, Bandcamp and Google Drive  music libraries all into one place.
-        </p>
+          <p className="company-description"> 
+           Fresh-track is a unique file repository that allows artists to listen to, share, and collaborate on their unreleased and released music from their Soundcloud, Bandcamp and Google Drive music libraries all into one place.
+          </p>
        
-        <div className="team-container">
-
-          <div className="team description">
-            <h2>ipsum dolorum</h2>
-                <img src='fresh-track-logo.png' alt="fresh-track-logo" />
-                  <div className="social-icons">
-                    <a href="https://github.com/fresh-track" className="GitHub social" target="_blank" rel="noopener noreferrer" >
-                       <FontAwesomeIcon icon={faGithub} size="2x" /></a>
-                </div>
-            </div>            
+        <div className="logo-container">
+          <div className="team-description">
+            <h2 className="get-started">Click on the fresh-track icon to get started!</h2>
+            <Link to='/player'><img className="corpo-logo" src='fresh-track-logo.png' alt="fresh-track-logo" /></Link>
+          </div>            
         </div>
       </div>
                    
